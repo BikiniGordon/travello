f757b8cd07156eb35eb5fbd21a798d99e4d4cf07
FrankIsNotAvailable <jagreeto2549@gmail.com>
2026-02-17 11:32:00 +0700

    MVC setup

---
+++ Travello/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Travello.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
