fb8876bb0dd50a416eb955842f6b94513a3ba54d
ggatatata <101575663+BikiniGordon@users.noreply.github.com>
2026-03-10 17:35:39 +0700

    fix poll icon and option

--- wwwroot/js/Chat.js
+++ wwwroot/js/Chat.js
@@ -579,7 +579,8 @@ function openCreatePoll(){
     createPollPage.style.display = 'flex';
     const createPollBody = document.getElementById('create-poll-body');
     animatePollSurface(createPollBody);
-    setupPollTimeDropdowns(); 
+    setupPollTimeDropdowns();
+    initializeAddOptionButton();
 
 }
 
